359d80fe669925fb84a90e3d0df892ebd20840d4
shraddha-sadavarte <shraddhasadavarte24@gmail.com>
2022-12-10 20:02:13 -0800

    modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,54 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
 
   const [emoji, setEmoji]=useState("🍕")
-  const[emojiColor, setEmojiColor]=useState('black')
+  const[emojiColor, setEmojiColor]=useState('lavendor')
   const [emojiSize, setEmojiSize] = useState(100);
   const [emojiDeg, setEmojiDeg]=useState(0)
+  
+
+useEffect(()=>{
+    const a=localStorage.getItem("emoji");
+    const b=localStorage.getItem("emojiColor");
+    const c=localStorage.getItem("emojiSize");
+    const d=localStorage.getItem("emojiDeg");
+
+    if(a){
+      setEmoji(a);
+    }
+
+    if(b){
+      setEmojiColor(b);
+    }
+
+    if(c){
+      setEmojiSize(c);
+    }
+
+    if(d){
+      setEmojiDeg(d);
+    }
+},[])
+
+  useEffect(()=>{
+    if(emoji){
+      localStorage.setItem("Emoji",emoji);
+    }
+
+    if(emojiColor){
+      localStorage.setItem("EmojiColor",emojiColor);
+    }
+
+    if(emojiSize){
+      localStorage.setItem("EmojiSize",emojiSize);
+    }
+
+    if(emojiDeg){
+      localStorage.setItem("Emoji",emojiDeg);
+    }
+  }, [emoji,emojiColor,emojiSize,emojiDeg])
   return (
     
       <div className='container'>
@@ -20,6 +62,7 @@ function App() {
                  {emoji}
              </div>
           </div>
+
         <div>
           <div className='emoji-container'>
             <div className='emoji-item' onClick={()=>{setEmoji('🍕')}}>🍕</div>
@@ -30,7 +73,7 @@ function App() {
           </div>
         </div>
 
-        
+           <div>
           <div className='colors-container'>
             <div className='color-item bg-salmon' onClick={()=>{setEmojiColor("salmon")}}></div>
             <div className='color-item bg-lavendor' onClick={()=>{setEmojiColor("lavendor")}}></div>
@@ -47,9 +90,8 @@ function App() {
             <input type="range" className='size-slider' onChange={(e)=>{setEmojiDeg(e.target.value)}}/>
           </div>
         </div>
-        </div>
-        
-      
+       </div>
+       </div>
       </div>
   
   );
